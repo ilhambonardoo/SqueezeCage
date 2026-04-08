bebf9e2f8423d046f9125bb507ec0333d18df6b4
ilhambonardoo <ilhambonardo123@gmail.com>
2026-04-08 21:05:20 +0700

    Membuat bussines logic untuk authentication menggunakan next auth dengan token JWT

---
+++ auth.ts
@@ -0,0 +1,84 @@
+import { prisma } from "@/src/lib/prisma";
+import { compare } from "bcryptjs";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    Credentials({
+      name: "Sign in",
+      credentials: {
+        identifier: {
+          label: "Email atau username",
+          type: "text",
+          placeholder: "Tulis email atau username disini!",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.identifier || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findFirst({
+          where: {
+            OR: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          nama: user.nama,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.username = user.username;
+        token.nama = user.nama;
+        token.role = user.role;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.username = token.username;
+        session.user.nama = token.nama;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handlers = NextAuth(authOptions);
+
+export { handlers as GET, handlers as POST };
