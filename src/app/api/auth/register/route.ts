0d328e45e9cf9128195e761aaf47df07f7550879
ilhambonardoo <ilhambonardo123@gmail.com>
2026-05-21 22:29:20 +0700

    Menambahkan validasi untuk register pada saat pembuatan akun dan mengubah catch menjadi serverError(error)

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,9 +1,27 @@
+import { UserValidation } from "@/src/lib/validation/user-validation";
 import { register } from "@/src/services/auth-services";
+import { serverError } from "@/src/utils/api-helper";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
+
+    const validation = UserValidation.CREATE.safeParse({
+      ...body,
+      role: "OPERATOR",
+    });
+
+    if (!validation.success) {
+      return NextResponse.json(
+        {
+          message: "Data tidak valid",
+          errors: validation.error.flatten((issue) => issue.message)
+            .fieldErrors,
+        },
+        { status: 400 },
+      );
+    }
 
     const response = await register(body);
 
@@ -14,12 +32,7 @@ export async function POST(request: Request) {
       );
     }
     return NextResponse.json({ message: response.message }, { status: 201 });
-  } catch {
-    return NextResponse.json(
-      {
-        message: "Terjadi kesalahan pada server",
-      },
-      { status: 500 },
-    );
+  } catch (error) {
+    return serverError(error);
   }
 }
