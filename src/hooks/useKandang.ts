491891ee1206d372ef8fc5cb3096f277b89fb430
ilhambonardoo <ilhambonardo123@gmail.com>
2026-06-20 13:12:32 +0700

    Membuat customhooks untuk useKandang dan useSekat

---
+++ src/hooks/useKandang.ts
@@ -0,0 +1,100 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { KandangWithSekat } from "../interface/kandang-sekat";
+import toast from "react-hot-toast";
+
+export function useKandang() {
+  const [dataKandang, setDataKandang] = useState<KandangWithSekat[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitting, setIsSubmmiting] = useState(false);
+  const [errors, setErrors] = useState("");
+
+  const isFetched = useRef(false);
+
+  const getDataKandang = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch("/api/kandang");
+      const data = await res.json();
+
+      if (!res.ok) {
+        setErrors("Gagal mengambil data kandang");
+        return;
+      }
+
+      setDataKandang(data);
+    } catch {
+      setErrors("Terjadi kesalahan pada jaringan");
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isFetched.current) return;
+    isFetched.current = true;
+
+    getDataKandang();
+  }, [getDataKandang]);
+
+  const createDataKandangNew = async (nama: string) => {
+    try {
+      setIsSubmmiting(true);
+      const res = await fetch("/api/kandang", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ nama }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message || "Gagal membuat kandang!");
+        return { success: false };
+      }
+
+      toast.success("Kandang berhasil dibuat!");
+      await getDataKandang(); // Memanggil data kandang agar sinkron pada sat diupdate
+
+      return { success: true };
+    } catch {
+      setErrors("Terjadi kesalahan pada jaringan");
+    } finally {
+      setIsSubmmiting(false);
+    }
+  };
+
+  const deleteDataKandang = async (id: string) => {
+    try {
+      setIsSubmmiting(true);
+
+      const res = await fetch(`/api/kandang/${id}`, { method: "DELETE" });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message || "Gagal menghapus data kandang");
+        return { success: false };
+      }
+
+      setDataKandang((prevData) =>
+        prevData.filter((kandang) => kandang.id !== id),
+      );
+
+      return { success: true };
+    } catch {
+      toast.error("Terjadi kesalahan jaringan saat menghapus data");
+      return { success: false };
+    } finally {
+      setIsSubmmiting(false);
+    }
+  };
+
+  return {
+    dataKandang,
+    isLoading,
+    isSubmitting,
+    errors,
+    getDataKandang,
+    createDataKandangNew,
+    deleteDataKandang,
+  };
+}
